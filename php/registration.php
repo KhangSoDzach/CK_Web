2ec03e20ea35490dddb2b7c55cce3ab32d7e7ad1
KhangSoDzach <exal799@gmail.com>
2024-03-16 14:10:52 +0700

    Update lai moi login va register

---
+++ php/registration.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style1.css">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["submit"])) {
+           $username = $_POST["username"];
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+           $passwordRepeat = $_POST["repeat_password"];
+           
+           $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+           $errors = array();
+           
+           if (empty($username) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+            array_push($errors,"All fields are required");
+           }
+           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($errors, "Email is not valid");
+           }
+           if (strlen($password)<8) {
+            array_push($errors,"Password must be at least 8 charactes long");
+           }
+           if ($password!==$passwordRepeat) {
+            array_push($errors,"Password does not match");
+           }
+           require_once "database.php";
+           $sql = "SELECT * FROM user WHERE email = '$email'";
+           $result = mysqli_query($conn, $sql);
+           $rowCount = mysqli_num_rows($result);
+           if ($rowCount>0) {
+            array_push($errors,"Email already exists!");
+           }
+           if (count($errors)>0) {
+            foreach ($errors as  $error) {
+                echo "<div class='alert alert-danger'>$error</div>";
+            }
+           }else{
+            
+            $sql = "INSERT INTO user (username, email, password) VALUES ( ?, ?, ? )";
+            $stmt = mysqli_stmt_init($conn);
+            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+            if ($prepareStmt) {
+                mysqli_stmt_bind_param($stmt,"sss",$username, $email, $passwordHash);
+                mysqli_stmt_execute($stmt);
+                echo "<div class='alert alert-success'>You are registered successfully.</div>";
+            }else{
+                die("Something went wrong");    
+            }
+           }
+          
+
+        }
+        ?>
+        <form action="registration.php" class="my-form" method="post">
+            <div class="login-welcome-row">
+                <a href="#" title="Logo">
+                    <img src="assets/storeify.png" alt="Logo" class="logo">
+                </a>
+                <h1>WELCOME &#x1F44F;</h1>
+                <p>Please enter your details!</p>
+            </div>
+            <div class="input__wrapper">
+                <input type="username" id="usename" name="username" class="input__field" placeholder="Your Name" required>
+                <label for="username" class="input__label">Username:</label>
+                <svg class="input__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+                <path d="M6.5 21h-2a1 1 0 0 1 -1 -1v-3.5"></path>
+                <path d="M18.5 21h2a1 1 0 0 0 1 -1v-3.5"></path>
+                <path d="M15 11a3.5 3.5 0 1 0 -6 0"></path>
+            </svg>
+            
+            </div>
+            <div class="input__wrapper">
+                <input type="email" id="email" name="email" class="input__field" placeholder="Your Email" required>
+                <label for="email" class="input__label">Email:</label>
+                <svg class="input__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                    <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"></path>
+                </svg>
+            </div>
+            <div class="input__wrapper">
+                <input id="password" type="password" class="input__field" name="password" placeholder="Your Password"
+                    title="Minimum 6 characters at least 1 Alphabet and 1 Number"
+                    pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$" required>
+                <label for="password" class="input__label">
+                    Password
+                </label>
+                <svg class="input__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z"></path>
+                    <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0"></path>
+                    <path d="M8 11v-4a4 4 0 1 1 8 0v4"></path>
+                </svg>
+            </div>
+            <div class="input__wrapper">
+                <input id="password" type="password" class="input__field" name="repeat_password" placeholder="Your Password"
+                    title="Minimum 6 characters at least 1 Alphabet and 1 Number"
+                    pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$" required>
+                <label for="password" class="input__label">
+                    Repeat Password
+                </label>
+                <svg class="input__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z"></path>
+                    <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0"></path>
+                    <path d="M8 11v-4a4 4 0 1 1 8 0v4"></path>
+                </svg>
+            </div>
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
+            </div>
+        </form>
+        <div>
+        <div><p>Already Registered <a href="login.php">Login Here</a></p></div>
+      </div>
+    </div>
+</body>
+</html>
